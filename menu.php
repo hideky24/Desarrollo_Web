9987b6cd85a48685ceb243ac591f7e40cf8a5264
hideky24 <88519929+hideky24@users.noreply.github.com>
2022-08-15 22:51:31 -0500

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,23 @@
+<!--cliente/index.php"> Vizualizar agenda </a>
+        >cliente/add.php"> Venta de inmuebles </a>
+        </li>
+    </ul> 
+</nav> -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="../index.php">
+                    <img src="../img/logo.webp" href="../index.php" alt="" height="100px" >
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav ml-auto">
+                    <a class="nav-item nav-link active" href="#">INICIO <span class="sr-only"></span></a>
+                    <a class="nav-item nav-link" href="https://es.wikipedia.org/wiki/Ubal%C3%A1#Historia">HISTORIA</a>
+                    <a class="nav-item nav-link" href="../html/pagina2.php">SITIOS TURISTICOS</a>
+                    <a class="nav-link" href="..//cliente/add.php"> VENTA DE INMUEBLES </a>
+                  </div>
+                </div>
+            </div>
+        </nav>
